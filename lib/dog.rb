4fd457727eee7843d5cb0ad96d307b48357f39c1
Nathan Rathe <nprathe10@gmail.com>
2019-07-23 15:09:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Add your code here
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each { |n| puts n.name}
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end
